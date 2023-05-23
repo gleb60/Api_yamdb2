4ee44200faa055cb264cb1dfa9c1aba3f07d1a58
Snake <snake85@list.ru>
2023-05-23 16:19:04 -0600

    reviews.models add class Comment

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from users.models import User
 
 # Create your models here.
+class Reviews(models.Model):
+
+
+# The model of comments to reviews.
+class Comment(models.Model):
+
+    reviews = models.ForeignKey(
+        'Review',
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='review',
+        blank=True,
+        null=True,
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Author',
+    )
+    text = models.TextField(
+        verbose_name='comments',
+        help_text='write your comment'
+    )
+
+
+    class Meta:
+        ordering = ('-pub_date',)
+        verbose_name = 'Comment',
+        verbose_name_plural = 'Comments'
