2ed70ff53ff4709e3731ebd362798c719668231f
gleb60 <dr.buldog@yandex.ru>
2023-05-25 16:37:21 +0200

    Почти закончена регистрация пользователя, отправка письма и токен

--- api_yamdb/users/urls.py
+++ api_yamdb/users/urls.py
@@ -6,6 +6,7 @@ from .views import RegistrationView, UserViewSet
 router = routers.DefaultRouter()
 router.register('users', UserViewSet)
 
+
 urlpatterns = [
     path('v1/auth/signup/', RegistrationView.as_view(), name='registration'),
     path('v1/auth/token/', include('djoser.urls')),
