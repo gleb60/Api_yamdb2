b570eb19d642b1664cfa13fea0ee5efd11d0b9f0
Snake <snake85@list.ru>
2023-05-25 22:05:00 -0600

    начало работы с api

---
+++ api_yamdb/api/urls.py
@@ -0,0 +1,10 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+from .views import ReviewViewSet
+
+router_v1 = DefaultRouter()
+router_v1.register(r'reviews', ReviewViewSet)
+
+urlpatterns = [
+    path('', include(router_v1.urls))
+]
