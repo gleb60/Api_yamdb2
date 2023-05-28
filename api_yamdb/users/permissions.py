c742fa0761a81b0baea7ae193e7126f89b4b713a
gleb60 <dr.buldog@yandex.ru>
2023-05-28 18:00:59 +0200

    Закончил, все 47 моих тестов проходит.

--- api_yamdb/users/permissions.py
+++ api_yamdb/users/permissions.py
@@ -1,7 +1,10 @@
 from rest_framework import permissions
+from rest_framework.exceptions import MethodNotAllowed
 
 
 class IsAdminOrSuperuser(permissions.BasePermission):
 
     def has_permission(self, request, view):
-        return request.user.is_admin
+        if request.method == 'PUT':
+            raise MethodNotAllowed('PUT')
+        return request.user.is_authenticated and request.user.is_admin
