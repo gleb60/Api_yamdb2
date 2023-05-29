7d203e01dfeb2f7b7bc9d021559949df41a2773b
Snake <snake85@list.ru>
2023-05-29 15:53:08 -0600

    final_v3

---
+++ api_yamdb/api/serializers.py
@@ -0,0 +1,108 @@
+from django.forms import ValidationError
+from django.shortcuts import get_object_or_404
+from rest_framework import serializers
+import datetime as dt
+from reviews.models import Comment, Review, Title, Category, Genre
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        lookup_field = 'slug'
+        fields = ('name', 'slug')
+
+    def validate_slug(self, value):
+        if self.instance and self.instance.slug == value:
+            return value
+        if Category.objects.filter(slug=value).exists():
+            raise serializers.ValidationError('Slug must be unique.')
+        return value
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        lookup_field = 'slug'
+        fields = ('name', 'slug')
+
+    def validate_slug(self, value):
+        if self.instance and self.instance.slug == value:
+            return value
+        if Genre.objects.filter(slug=value).exists():
+            raise serializers.ValidationError('Slug must be unique.')
+        return value
+
+
+class TitleGetSerializer(serializers.ModelSerializer):
+    genre = GenreSerializer(many=True)
+    category = CategorySerializer()
+
+    class Meta:
+        model = Title
+        fields = ('id', 'name', 'year', 'rating', 'description', 'genre',
+                  'category')
+
+    def validate_year(self, value):
+        """Проверяет, что год выпуска раньше текущего"""
+        year = dt.date.today().year
+        if (value < year):
+            raise serializers.ValidationError(
+                'Дата не может быть позднее текущей'
+            )
+        return value
+
+
+class TitleWriteSerializer(serializers.ModelSerializer):
+    genre = serializers.SlugRelatedField(
+        slug_field='slug', many=True, queryset=Genre.objects.all()
+    )
+    category = serializers.SlugRelatedField(
+        slug_field='slug', queryset=Category.objects.all()
+    )
+
+    class Meta:
+        model = Title
+        fields = ('__all__')
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+
+    author = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username'
+    )
+    title = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='name'
+    )
+
+    def validate(self, data):
+        request = self.context['request']
+        author = request.user
+        title_id = self.context['view'].kwargs.get('title_id')
+        title = get_object_or_404(Title, pk=title_id)
+        if request.method == 'POST':
+            if Review.objects.filter(title=title, author=author).exists():
+                raise ValidationError('Вы не можете добавить более'
+                                      'одного отзыва на произведение')
+        return data
+
+    class Meta:
+        fields = '__all__'
+        model = Review
+
+
+class CommentSerializer(serializers.ModelSerializer):
+
+    author = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username'
+    )
+    review = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='text'
+    )
+
+    class Meta:
+        fields = '__all__'
+        model = Comment
