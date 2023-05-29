470ec9e5ae66984a6714d358d2d3daf5d03835e9
Gromtag <gromtag@yandex.ru>
2023-05-29 22:54:48 +0200

    YAMDB_Final

--- README.md
+++ README.md
@@ -1,3 +1,65 @@
-# api_yamdb
-api_yamdb
-### Для пайтеста
+# Api YAMDB
+## Технологии
+- Python 3.9
+- Django 3.2
+- Django REST framework 3.12.4
+## Описание проекта
+Апи разработано для проекта YAMDB. 
+Оно позволяет выставлять рейтинги контенту, оставлять отзывы и комментировать их.
+ 
+### Как запустить проект:
+ 
+Клонировать репозиторий и перейти в него в командной строке:
+ 
+```
+git clone git@github.com:Gromtag/api_yamdb.git
+```
+ 
+```
+cd api_yamdb
+```
+ 
+Cоздать виртуальное окружение на Linux:
+ 
+```
+python3 -m venv venv
+```
+ 
+Или на Windows:
+ 
+```
+python -m venv venv
+```
+ 
+И активировать виртуальное окружение на Linux:
+```
+. venv/bin/activate
+```
+Или на Windows:
+```
+source venv/Scripts/activate
+```
+Установить зависимости из файла requirements.txt:
+ 
+```
+python3 -m pip install --upgrade pip
+```
+ 
+```
+pip install -r requirements.txt
+```
+Зайти в папку приложения:
+```
+cd api_yamdb
+```
+Выполнить миграции:
+ 
+```
+python3 manage.py migrate
+```
+ 
+Запустить проект:
+ 
+```
+python3 manage.py runserver
+```
--- api_yamdb/scripts/load_data.py
+++ api_yamdb/scripts/load_data.py
@@ -1,21 +1,30 @@
-from django.core.management.base import BaseCommand
-from reviews.models import Title, Genre, Category, Review, Comment
 import csv
-
-
-class Command(BaseCommand):
-    def handle(self, *args, **options):
-        self.load_titles()
-
-
-    def load_titles(self, file_path):
-        with open('static/data/titles.csv', newline='') as file:
-            reader = csv.DictReader(file)
-            for row in reader:
-                title = Title()
-                title.name = row['name']
-                title.year = int(row['year'])
-                title.description = row['description']
-                title.category = row['category']
-                title.genre = row['genre']
-                title.save()
+import sqlite3
+ 
+# Подключение к базе данных
+conn = sqlite3.connect('api_yamdb\db.sqlite3')
+cursor = conn.cursor()
+ 
+# Список файлов CSV для загрузки
+static_data = ['api_yamdb\static\data\category.csv',
+             'api_yamdb\static\data\comments.csv',
+             'api_yamdb\static\data\genre_title.csv',
+             'api_yamdb\static\data\genre.csv',
+             'api_yamdb\static\data\review.csv',
+             'api_yamdb\static\data\titles.csv',
+             'api_yamdb\static\data\users.csv',]
+ 
+# Цикл по файлам CSV
+for csv_data in static_data:
+    # Чтение данных из CSV файла
+    with open(csv_data, 'r') as data:
+        csv_reader = csv.reader(data)
+        next(csv_reader)  # Пропуск заголовка, если он есть
+ 
+        # Цикл по строкам CSV файла
+        for row in csv_reader:
+            cursor.execute('INSERT INTO table_name (column1, column2, column3) VALUES (?, ?, ?)', (row[0], row[1], row[2]))
+ 
+# Сохранение изменений и закрытие соединения с базой данных
+conn.commit()
+conn.close()
